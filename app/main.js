08092b33e16266c6ee5c03c7df6892683557d155
Simranjeet.Singh1 <Simranjeet.singh1@rci.rogers.ca>
2024-11-07 14:25:17 -0500

    [get set]

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const storage = {}
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -10,6 +11,16 @@ const server = net.createServer((connection) => {
     const commands = data.toString().split('\r\n');
     if(commands[2].toLowerCase() === 'echo'){
         return connection.write('$'+commands[4].length+'\r\n'+commands[4]+'\r\n');
+    }
+    if(commands[2].toLowerCase() === 'set'){
+        storage[commands[4]] = commands[6];
+        return connection.write('+OK\r\n');
+    }
+    if(commands[2].toLowerCase() === get){
+        if(storage[commands[4]].length){
+        return connection.write('$'+storage[commands[4]].length+'\r\n'+storage[commands[4]]+'\r\n');
+        }
+        return connection.write('$-1\r\n');
     }
     connection.write('+PONG\r\n');
   })
