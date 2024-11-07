c9aef4dd894a8a6bedc862358cdfac4f3365a0a1
Simranjeet.Singh1 <Simranjeet.singh1@rci.rogers.ca>
2024-11-07 13:14:46 -0500

    [testing multiple  pings]

--- app/main.js
+++ app/main.js
@@ -6,7 +6,6 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.write('+PONG\r\n');
   connection.on('data',()=>{
     connection.write('+PONG\r\n');
   })
