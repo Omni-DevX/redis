ac79a7d3a24992e66592478737fd37a9762bcc4e
Simranjeet.Singh1 <Simranjeet.singh1@rci.rogers.ca>
2024-11-27 00:15:06 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -31,7 +31,7 @@ fs.readFile(join(config.get('dir'),config.get('dbfilename')), (err, data)=>{
         console.log(err);
         return;
     }
-    console.log(data);
+    console.log(data[i]);
 })
 
 // Uncomment this block to pass the first stage
